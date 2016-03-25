b6ebc65926385f4aed0c27e50cf825d02e8d85ef
wangquantcoder <wangquantcoder@gmail.com>
2016-03-25 21:22:36 +0800

    split the source image in YUV format into Y, U, V separately.
    split the source image in YUV format into Y, U, V separately.

---
+++ simplest_yuv420_split code/main.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+#include"simplest_yuv420_split.h"
+int main()
+{
+
+simplest_yuv420_split("lena_256x256_yuv420p.yuv", 256, 256, 1);
+
+return 0;
+
+}
---
+++ simplest_yuv420_split code/simplest_yuv420_split.h
@@ -0,0 +1,4 @@
+#ifndef SIMPLEST_YUV420_SPLIT_H_
+#define SIMPLEST_YUV420_SPLIT_H_
+int simplest_yuv420_split(char * url, int w, int h, int num);
+#endif
